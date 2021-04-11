7c45ddd22cb5e3007ecc5cc561a6ee6ca9da178e
Abhay G Deshpande <abhaygirish1997@gmail.com>
2021-04-11 13:04:06 +0530

    changes to refresh token

--- src/index.ts
+++ src/index.ts
@@ -4,12 +4,39 @@ import { ApolloServer } from "apollo-server-express";
 import { buildSchema } from "type-graphql";
 import { UserResolver } from "./UserResolver";
 import { createConnection } from "typeorm";
+import {verify} from "jsonwebtoken"
+import CookieParser from "cookie-parser"
 import "dotenv/config";
+import { User } from "./entity/User";
+import { createAccessToken } from "./auth";
 (async () => {
   const app = express();
+  app.use(CookieParser())
   app.get("/", (_req, res) => {
     res.send("Hello");
   });
+  app.post("/refresh_token",async (req,res)=>{
+    const token=req.cookies;
+    if(!token){
+      return res.send({ok:false,accessToken:""})
+    }
+    let payload:any=null
+    try{
+      payload=verify(token,process.env.REFRESH_TOKEN_SECRET!)
+    }
+    catch(err){
+      console.log("hitting", token,err);
+      return res.send({ok:false,accessToken:""})
+    }
+
+    //token is valid
+    const user=await User.findOne({id:payload.userId})
+    if(!user){
+      return res.send({ok:false,accessToken:""})
+    }
+    return res.send({ok:true,accessToken:createAccessToken(user)})
+
+  })
   await createConnection();
   const apolloServer = new ApolloServer({
     schema: await buildSchema({
